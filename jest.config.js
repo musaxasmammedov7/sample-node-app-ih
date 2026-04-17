47360339b7a7ba844b4c1394bb6049c87abc5c46
musaxasmammedov7 <musaxasmammedov7@icloud.com>
2026-04-17 18:33:15 +0400

    Refactor jest.config.js for improved structure

--- jest.config.js
+++ jest.config.js
@@ -1,50 +1,34 @@
 module.exports = {
-  // Глобальные настройки
   reporters: [
-    'default',                    // стандартный вывод в консоль
-    ['jest-junit', {              // JUnit reporter для GitHub
-      outputDirectory: 'reports/junit',
+    'default',
+    ['jest-junit', {
+      outputDirectory: './reports/junit',
       outputName: 'junit.xml',
       addFileAttribute: 'true',
       suiteName: 'Jest Tests',
       classNameTemplate: '{classname}',
       titleTemplate: '{title}',
+      ancestorSeparator: ' › ',
+      usePathForSuiteName: true
     }]
   ],
 
-  coverageDirectory: 'coverage',
-  collectCoverage: true,        // можно включить глобально
+  coverageDirectory: './coverage',
 
-  // Проекты
   projects: [
     {
       displayName: 'server',
       testEnvironment: 'node',
-      testMatch: [
-        '**/tests/server.test.js',
-        '**/tests/integration.test.js'
-      ],
-      collectCoverageFrom: [
-        'server.js',
-        '!**/node_modules/**',
-        '!**/coverage/**',
-        '!**/reports/**'
-      ],
+      testMatch: ['**/tests/server.test.js', '**/tests/integration.test.js'],
+      collectCoverageFrom: ['server.js', '!**/node_modules/**', '!**/coverage/**', '!**/reports/**'],
       coverageDirectory: 'coverage/server',
       setupFilesAfterEnv: ['<rootDir>/tests/setup.js']
     },
     {
       displayName: 'client',
       testEnvironment: 'jsdom',
-      testMatch: [
-        '**/tests/client.test.js'
-      ],
-      collectCoverageFrom: [
-        'public/script.js',
-        '!**/node_modules/**',
-        '!**/coverage/**',
-        '!**/reports/**'
-      ],
+      testMatch: ['**/tests/client.test.js'],
+      collectCoverageFrom: ['public/script.js', '!**/node_modules/**', '!**/coverage/**', '!**/reports/**'],
       coverageDirectory: 'coverage/client',
       setupFilesAfterEnv: ['<rootDir>/tests/setup.js']
     }
